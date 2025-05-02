e86158993302da7904552294e2c11be374daef2d
oEnzoCastro <enzocastru@gmail.com>
2025-05-02 11:41:44 -0300

    ADD:  Carousel

--- src/node.js
+++ src/node.js
@@ -4,10 +4,13 @@ fetch("videos.json")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((video) => {
-      carousel.innerHTML += `
+      carousel.innerHTML +=
+        `
         
         <div class="carouselCard">
+        <h1>
         ` +
+        video.prof_nome +
         // <div class="carouselVideo">
         //     <iframe
         //     src="` + video.video_link + `"
@@ -17,13 +20,61 @@ fetch("videos.json")
         // </div>
         //     <h1 class="carouselDesc">` + video.video_desc + `</h1>
         //     <a href="` + video.prof_link + `" target="_blank">` + video.prof_nome + `</a>
-            `
+        `
+        </h1>
         </div>
 
         `;
     });
 
     console.log(data);
+    // Make the carousel move
+
+    let items = document.querySelectorAll(".carouselContainer .carouselCard");
+    let nextCard = document.getElementById("carouselNext");
+    let prevCard = document.getElementById("carouselPrev");
+
+    let active = 3;
+
+    function loadShow() {
+      let stt = 0;
+
+      items[active].style.transform = `none`;
+      items[active].style.zIndex = "1";
+      items[active].style.filter = "none";
+      items[active].style.opacity = 1;
+
+      for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(calc(${120 * stt}px)) scale(${
+          1 - 0.2 * stt
+        })`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(1px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+
+      stt = 0;
+      for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${
+          1 - 0.2 * stt
+        })`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(1px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+    }
+
+    loadShow();
+
+    nextCard.onclick = function() {
+      active = active + 1 < items.length ? active + 1 : active;
+      loadShow()
+    }
+    prevCard.onclick = function() {
+      active = active - 1 >= 0 ? active - 1 : active;
+      loadShow()
+    }
   });
 
-//   <iframe width="1236" height="695" src="https://www.youtube.com/embed/GF44ENIWRW0" title="Subaru Beetle 2.0 Turbo, double A suspension and all customized!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
