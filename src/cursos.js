adfdd399c9a60e41096b36e0c78b35bf9681b7a2
Silas <silasrviana@gmail.com>
2025-05-19 12:54:29 -0300

    correções de importação

--- src/cursos.js
+++ src/cursos.js
@@ -13,35 +13,35 @@ function importarCursos() {
       dados.forEach(categoria => {
         html += `
           <div class="textotopico">
-            <h1>nome do tópico: ${categoria.categoria}</h1>
+            <h1>${categoria.categoria}</h1>
             <div class="qtdCursos">
-              <h1>${categoria.cursos.length} cursos encontrados</h1>
+              <h1>${categoria.cursos.length}</h1>
             </div>
           </div>
         `;
 
+        html += `<div class="conjunto">`;  // abre um conjunto de cards
         categoria.cursos.forEach(curso => {
-          html += `
-            <div class="cursosContainer">
-              <div class="cursoCard">
+            html += `
+                <div class="cursoCard">
                 <label class="compararLabel">
-                  <input type="checkbox" />
+                    <input type="checkbox" />
                 </label>
 
                 <div class="cursoCardTop">
-                  <span class="Modalidade">${curso.modalidade}</span>
+                    <span class="Modalidade">${curso.modalidade}</span>
                 </div>
 
                 <div class="cursoBody">
-                  <h3>${curso.nome_do_curso}</h3>
-                  <img src="/assets/images/placeHolderCoureseCardImage.png" />
+                    <h3>${curso.nome_do_curso}</h3>
+                    <img src="/assets/images/placeHolderCoureseCardImage.png" />
                 </div>
 
                 <button class="btnDetalhes">Detalhes</button>
-              </div>
-            </div>
-          `;
+                </div>
+            `;
         });
+        html += `</div>`; // fecha linha depois do loop
       });
 
       section.innerHTML = html;
